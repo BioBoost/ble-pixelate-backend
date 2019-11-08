ab444510db612b6bf240ff1c72844885f376786c
Nico De Witte <nico.dewitte@outlook.com>
2019-11-08 16:49:45 +0100

    Start of socket events

--- src/server.js
+++ src/server.js
@@ -1,1 +1,21 @@
-console.log("Hello");
+const io = require('socket.io')();
+
+io.on('connection', client => {
+  console.log('Frontend connected');
+
+  client.on('start', (controllers) => {
+    console.log(`Starting the game with the following controller ${controllers}`);
+  })
+
+  client.on('update', (updates) => {
+    updates.forEach((update) => {
+      console.log(`Updates for controller ${update.controller}`);
+    })
+  })
+  
+  client.on('disconnect', function(){
+    console.log('Frontend disconnected');
+  });
+});
+
+io.listen(3000);
