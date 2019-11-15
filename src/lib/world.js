a94ad2ff1e447b1a7dbdac065684573994c4ce0f
Nico De Witte <nico.dewitte@outlook.com>
2019-11-15 20:38:27 +0100

    Basic render of world based on claimed fields

--- src/lib/world.js
+++ src/lib/world.js
@@ -6,13 +6,21 @@ class World {
   }
 
   init() {
-    this.playfield = Array(this.width).fill(null).map(f => Array(this.height).fill(null));
+    this.playfield = Array(this.width).fill(null).map(field => Array(this.height).fill(null));
   }
 
   render(display) {
-    // this renders the playfield on the display
+    // This renders the playfield on the display
     // If field is not owned by player, we draw nothing
     // If owned we draw colored pixel at that location
+    for (let x = 0; x < this.width; x++) {
+      for (let y = 0; y < this.height; y++) {
+        let owner = this.playfield[x][y];
+        if (owner) {
+          display.pixel(x, y, "#00FF00");
+        }
+      }
+    }
   }
 
   update(delta) {
