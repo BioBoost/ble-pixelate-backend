fffc6bea2e4e28a2a844657936c587d29eac07f3
Nico De Witte <nico.dewitte@outlook.com>
2019-11-15 20:25:35 +0100

    Add player models

--- src/lib/game.js
+++ src/lib/game.js
@@ -3,18 +3,37 @@ const Player = require('./player');
 const FileRenderer = require('./renderers/file_renderer');
 const MemeTvRenderer = require('./renderers/meme_tv_renderer');
 const World = require('./world');
-
-const Engine = require('./pixel_engine');
+const PixelEngine = require('./pixel_engine');
 
 class Game {
+  static PLAYFIELD_WIDTH = 96;
+  static PLAYFIELD_HEIGHT = 64;
 
   constructor() {
-    this.engine = new PixelEngine();
+    // Setup the engine
+    this.display = new Display(Game.PLAYFIELD_WIDTH, Game.PLAYFIELD_HEIGHT);
+    this.display.add_renderer(new FileRenderer(`${__dirname}/test.png`));
+    this.engine = new PixelEngine(this.display);
 
-    // When set to go
+    // Create the models
+    this.world = new World();
+    this.engine.register_model(this.world);
+    this.players = [];
+  }
+
+  add_player(name, controllerId) {
+    this.players[controllerId] = new Player(name, this.world);
+    this.engine.register_model(this.players[controllerId]);
+  }
+
+  start() {
+    // Fire up the engine
     this.engine.init();
   }
 
+  stop() {
+    this.engine.kill();
+  }
 
 }
 
