9c7b96300b7d7dd9bd3ebe1157aa8dcdc37debc0
Nico De Witte <nico.dewitte@outlook.com>
2019-11-15 20:26:30 +0100

    Comment out websockets to test engine and game

--- src/server.js
+++ src/server.js
@@ -1,22 +1,20 @@
 const io = require('socket.io')();
 const Game = require('./lib/game');
 
+// Game start
 let game = new Game();
+let start = [
+  { controller_id: 'id_pinky', name: 'nico', color: "#FF0000" },
+  { controller_id: 'id_hulk', name: 'marki', color: "#00FF00" },
+  { controller_id: 'id_rdr2', name: 'johhny', color: "#0000FF" },
+];
 
-// // Game start
-// let game = new Game(false);
-// let start = [
-//   { controller_id: 'id_pinky', name: 'nico', color: "#FF0000" },
-//   { controller_id: 'id_hulk', name: 'marki', color: "#00FF00" },
-//   { controller_id: 'id_rdr2', name: 'johhny', color: "#0000FF" },
-// ];
-
-// start.forEach((player) => {
-//   game.add_player(player.name, player.controller_id, player.color);
-// });
+start.forEach((player) => {
+  game.add_player(player.name, player.controller_id, player.color);
+});
 
 
-// game.start();
+game.start();
 
 // // Updates are coming in
 // let updates = [
@@ -58,44 +56,44 @@ let game = new Game();
 //   });
 // });
 
-let game = null;
+// let game = null;
 
-io.on('connection', client => {
-  console.log('Frontend connected');
+// io.on('connection', client => {
+//   console.log('Frontend connected');
 
-  client.on('start', (players) => {
-    console.log(`Starting the game with the following players ${players}`);
-    game = new Game(false);
-    JSON.parse(players).forEach((player) => {
-      game.add_player(player.name, player.controller_id, player.color);
-    });
-    game.start();
-  });
+//   client.on('start', (players) => {
+//     console.log(`Starting the game with the following players ${players}`);
+//     game = new Game(false);
+//     JSON.parse(players).forEach((player) => {
+//       game.add_player(player.name, player.controller_id, player.color);
+//     });
+//     game.start();
+//   });
 
-  client.on('update', (updates) => {
-    console.log(`Getting updates ${updates}`);
+//   client.on('update', (updates) => {
+//     console.log(`Getting updates ${updates}`);
 
-    let updatesObj = JSON.parse(updates);
-    updatesObj.forEach((update) => {
-      update.actions.forEach((action) => {
-        switch (action) {
-          case 'D': game.move_down(update.id, 5); break;
-          case 'U': game.move_up(update.id, 5); break;
-          case 'L': game.move_left(update.id, 5); break;
-          case 'R': game.move_right(update.id, 5); break;
-          case 'B': game.explosion(update.id); break;
-          case 'A': game.laser(update.id); break;
-          case 'X': game.mark_x(update.id); break;
-        }
-      });
-    });
+//     let updatesObj = JSON.parse(updates);
+//     updatesObj.forEach((update) => {
+//       update.actions.forEach((action) => {
+//         switch (action) {
+//           case 'D': game.move_down(update.id, 5); break;
+//           case 'U': game.move_up(update.id, 5); break;
+//           case 'L': game.move_left(update.id, 5); break;
+//           case 'R': game.move_right(update.id, 5); break;
+//           case 'B': game.explosion(update.id); break;
+//           case 'A': game.laser(update.id); break;
+//           case 'X': game.mark_x(update.id); break;
+//         }
+//       });
+//     });
 
-    game.render();
-  });
+//     game.render();
+//   });
   
-  client.on('disconnect', function(){
-    console.log('Frontend disconnected');
-  });
-});
+//   client.on('disconnect', function(){
+//     console.log('Frontend disconnected');
+//   });
+// });
 
-io.listen(3000);
+// io.listen(3000);
